122249a994dbc8e1a7be2e39c3035e5c620f4848
Steve Saporta <sasaporta@gmail.com>
2012-12-29 01:52:56 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wordy::Application.config.secret_token = '804d0c64182b7b9faabfa5997321b4066bcdeefe2edc88f1d66b9cc5cf4958ff9ec87d5c9f68d544dbec46e410793d197cfaa3243e65e45b81d06409c79132c0'
